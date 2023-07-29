1ae0ef14d4e33198ff178871f791509fefb79886
Marzdevs <alzokarimarwa@gmail.com>
2023-07-29 12:04:50 -0700

    express new main

--- server.js
+++ server.js
@@ -2,13 +2,41 @@ const express = require('express')
 const app = express()
 const PORT = 8000
 
+const Princesses = {
+    'Snow White':{
+        'film': 'Snow White and the Seven Dwarfs',
+        'age': 14,
+        'enemy':'The Evil Queen'
+    },
+    'Ariel':{
+        'film': 'The Little Mermaids',
+        'age': 16,
+        'enemy':'Ursula'
+    },
+    'Rapunzel':{
+        'film': 'Tangled',
+        'age': 18,
+        'enemy':'Mother Gothel'
+    },
+
+}
 //get request
 app.get('/', (request, response) => {
     //direct url name
     response.sendFile(__dirname + '/index.html')
 })
 
+// send json
+app.get('/api/:princessName', (request, response) =>{
+    const princessName = request.params.princessName
+    if(Princesses[princessName]){
+        response.json(Princesses[princessName])
+    }else{
+        response.json(Princesses['Rapunzel'])
+    }
+})
+
 // listen on port 8000
 app.listen(PORT, () => {
-    console.log(`The server is running on ${PORT}! You better go catch  it!`)
+    console.log(`The server is running on port ${PORT}! You better go catch  it!`)
 })
