433aa121732ec29eda906505cf263db257f4a444
Marzdevs <alzokarimarwa@gmail.com>
2023-07-29 17:23:03 -0700

    updated main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ document.querySelector('button').addEventListener('click', apiRequest)
 async function apiRequest(){
     const rapperName = document.querySelector('input').value
     try{
-        const response = await fetch(`https://princess-names-api-6378560d194e.herokuapp.com/${princessName}`)
+        const response = await fetch(`https://princess-names-api-6378560d194e.herokuapp.com/api/${princessName}`)
         const data = await response.json()
 
         console.log(data)
