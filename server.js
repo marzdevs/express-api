7103124a493a59f31ddc6585e61770652b42021c
Marzdevs <alzokarimarwa@gmail.com>
2023-07-29 17:48:46 -0700

    preview added express static

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const cors = require('cors')
 const PORT = 8000
 
 app.use(cors())
-app.use(express.static('public'))
+
+app.use('/static', express.static('public'))
 
 const Princesses = {
     'snow white':{
