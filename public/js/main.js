c40dfb081a9eb1b64eb699aa851b8927cb4eb6dd
Marzdevs <alzokarimarwa@gmail.com>
2023-07-29 22:06:18 -0700

    MAIN FIXED

--- public/js/main.js
+++ public/js/main.js
@@ -1,13 +1,13 @@
 document.querySelector('button').addEventListener('click', apiRequest)
 
 async function apiRequest(){
-    const princessName = document.querySelector('input').value
+    const princessName = document.querySelector('#input').value
     try{
         const response = await fetch(`https://princess-names-api-6378560d194e.herokuapp.com/api/${princessName}`)
         const data = await response.json()
 
         console.log(data)
-        document.querySelector('h2').innerText = data.birthName
+        document.querySelector('.h2').innerText = data.birthName
     }catch(error){
         console.log(error)
     }
